09d95a462d7cf5e57f3fa157380c5e73c6b6417d
Abhinav Sancheti <001sancheti@gmail.com>
2021-08-03 19:46:41 +0530

    updated shop data with new data, updated cart utils to group item in cart reducer

--- dazzle-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ dazzle-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CollectionItem from '../collection-item/collection-item.collection';
+import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
@@ -11,11 +11,8 @@ const CollectionPreview = ({ title, items }) => {
             <div className='preview'>
                 {items
                     .filter((item, index) => index < 4)
-                    .map(({ id, ...otherItemProps }) => (
-                        <CollectionItem
-                            key={id}
-                            {...otherItemProps}
-                        ></CollectionItem>
+                    .map((item) => (
+                        <CollectionItem key={item.id} item={item} />
                     ))}
             </div>
         </div>
